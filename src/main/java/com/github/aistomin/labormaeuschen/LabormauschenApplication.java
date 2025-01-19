96a5e76b8fc07a9a205fc3916a0c9c0e50e97e95
aistomin <andrej.istomin.ikeen@gmail.com>
2025-01-19 06:00:47 +0100

    Issue #1. Application skeleton.

---
+++ src/main/java/com/github/aistomin/labormaeuschen/LabormauschenApplication.java
@@ -0,0 +1,12 @@
+package com.github.aistomin.labormaeuschen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabormauschenApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(LabormauschenApplication.class, args);
+	}
+}
