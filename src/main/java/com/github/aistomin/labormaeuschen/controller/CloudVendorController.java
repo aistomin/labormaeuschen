341ec9accf592a1c8b316046e813ac62a59904bd
aistomin <andrej.istomin.ikeen@gmail.com>
2025-01-20 11:08:06 +0100

    Issue #5. Simple POST API is implemented.

--- src/main/java/com/github/aistomin/labormaeuschen/controller/CloudVendorController.java
+++ src/main/java/com/github/aistomin/labormaeuschen/controller/CloudVendorController.java
@@ -2,6 +2,8 @@ package com.github.aistomin.labormaeuschen.controller;
 
 import com.github.aistomin.labormaeuschen.model.CloudVendor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cloud/vendor")
 public class CloudVendorController {
 
+    /**
+     * Sample object that we can set and get using REST API.
+     */
+    private CloudVendor vendor;
+
     @GetMapping("{id}")
     public CloudVendor cloudVendorDetails(final String id) {
-        return new CloudVendor(
-            "C1",
-            "Max Mustermann",
-            "Musterstr. 10\n80336 Munich\nGermany",
-            "+49 30 901820"
-        );
+        return vendor;
+    }
+
+    @PostMapping
+    public String createCloudVendor(final @RequestBody CloudVendor newVendor) {
+        vendor = newVendor;
+        return "SUCCESS";
     }
 }
