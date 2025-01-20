7dbc58c7c9ba8f49af1685ae0ec70601b3089366
aistomin <andrej.istomin.ikeen@gmail.com>
2025-01-20 14:47:26 +0100

    Issue #7. Simple PUT API is implemented.

--- src/main/java/com/github/aistomin/labormaeuschen/controller/CloudVendorController.java
+++ src/main/java/com/github/aistomin/labormaeuschen/controller/CloudVendorController.java
@@ -3,6 +3,7 @@ package com.github.aistomin.labormaeuschen.controller;
 import com.github.aistomin.labormaeuschen.model.CloudVendor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +17,38 @@ public class CloudVendorController {
      */
     private CloudVendor vendor;
 
+    /**
+     * Get vendor's details by id.
+     *
+     * @param id Vendor's ID.
+     * @return Vendor's details.
+     */
     @GetMapping("{id}")
     public CloudVendor cloudVendorDetails(final String id) {
         return vendor;
     }
 
+    /**
+     * Create a new vendor.
+     *
+     * @param vendor New vendor.
+     * @return Result message.
+     */
     @PostMapping
-    public String createCloudVendor(final @RequestBody CloudVendor newVendor) {
-        vendor = newVendor;
-        return "SUCCESS";
+    public String createCloudVendor(final @RequestBody CloudVendor vendor) {
+        this.vendor = vendor;
+        return "Cloud vendor added.";
+    }
+
+    /**
+     * Update an existing vendor.
+     *
+     * @param vendor Updated vendor.
+     * @return Result message.
+     */
+    @PutMapping
+    public String updateCloudVendor(final @RequestBody CloudVendor vendor) {
+        this.vendor = vendor;
+        return "Cloud vendor updated.";
     }
 }
