29c9791fbea1154f4c082d4b4aec0629c3a4d276
aistomin <andrej.istomin.ikeen@gmail.com>
2025-01-27 02:48:11 +0100

    Issue #11. Refactoring.

--- src/main/java/com/github/aistomin/labormaeuschen/controller/CloudVendorController.java
+++ src/main/java/com/github/aistomin/labormaeuschen/controller/CloudVendorController.java
@@ -1,22 +1,32 @@
 package com.github.aistomin.labormaeuschen.controller;
 
 import com.github.aistomin.labormaeuschen.model.CloudVendor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cloud/vendor")
 public class CloudVendorController {
 
     /**
-     * Sample object that we can set and get using REST API.
+     * Logger.
      */
-    private CloudVendor vendor;
+    private final Logger logger = LoggerFactory.getLogger(CloudVendorController.class);
+
+    /**
+     * Primitive demo storage.
+     */
+    private final List<CloudVendor> storage = new ArrayList<>();
 
     /**
      * Get vendor's details by id.
@@ -24,9 +34,13 @@ public class CloudVendorController {
      * @param id Vendor's ID.
      * @return Vendor's details.
      */
-    @GetMapping("{id}")
-    public CloudVendor cloudVendorDetails(final String id) {
-        return vendor;
+    @GetMapping("/{id}")
+    public CloudVendor cloudVendorDetails(final @PathVariable String id) {
+        logger.info("Search Cloud Vendor with id: {}", id);
+        return storage.stream()
+            .filter(cloud -> cloud.getId().equals(id))
+            .findFirst()
+            .orElse(null);
     }
 
     /**
@@ -37,7 +51,8 @@ public class CloudVendorController {
      */
     @PostMapping
     public String createCloudVendor(final @RequestBody CloudVendor vendor) {
-        this.vendor = vendor;
+        logger.info("Create Cloud Vendor with id: {}", vendor.getId());
+        storage.add(vendor);
         return "Cloud vendor added.";
     }
 
@@ -49,7 +64,9 @@ public class CloudVendorController {
      */
     @PutMapping
     public String updateCloudVendor(final @RequestBody CloudVendor vendor) {
-        this.vendor = vendor;
+        logger.info("Update Cloud Vendor with id: {}", vendor.getId());
+        deleteVendorDetails(vendor.getId());
+        createCloudVendor(vendor);
         return "Cloud vendor updated.";
     }
 
@@ -59,9 +76,11 @@ public class CloudVendorController {
      * @param id Vendor's ID.
      * @return Result message.
      */
-    @DeleteMapping("{id}")
-    public String deleteVendorDetails(final String id) {
-        this.vendor = null;
+    @DeleteMapping("/{id}")
+    public String deleteVendorDetails(final @PathVariable String id) {
+        logger.info("Delete Cloud Vendor with id: {}", id);
+        final var old = cloudVendorDetails(id);
+        storage.remove(old);
         return "Cloud vendor is deleted.";
     }
 }
